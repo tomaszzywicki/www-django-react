cf234312aa67cd3eb032fce0f048a849c5b4cc9b
tomaszzywicki <tomasz.zywicki03@gmail.com>
2025-01-27 18:58:13 +0100

    book description css

--- frontend/src/components/Book.jsx
+++ frontend/src/components/Book.jsx
@@ -9,6 +9,9 @@ import CommentsSection from "./CommentsSection";
 const Book = () => {
   const { id } = useParams();
   const [book, setBook] = useState(null);
+  const [isExpanded, setIsExpanded] = useState(false); // Dodany stan
+
+  const maxDescriptionLength = 600;
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -23,18 +26,52 @@ const Book = () => {
     fetchBook();
   }, [id]);
 
-  if (!book) return <div>Loading...</div>;
+  if (!book) return <div className="book-loading">Loading...</div>;
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const renderDescription = () => {
+    if (book.description.length <= maxDescriptionLength) {
+      return <p className="book-description">{book.description}</p>;
+    }
+
+    if (isExpanded) {
+      return (
+        <p className="book-description">
+          {book.description}{" "}
+          <span className="read-more" onClick={toggleDescription}>
+            Read less
+          </span>
+        </p>
+      );
+    } else {
+      return (
+        <p className="book-description">
+          {book.description.substring(0, maxDescriptionLength)}...{" "}
+          <span className="read-more" onClick={toggleDescription}>
+            Read more
+          </span>
+        </p>
+      );
+    }
+  };
 
   return (
-    <div className="book-details">
-      <img className="book-cover" src={book.cover_image} alt={book.title} />
-      <div className="book-info">
-        <h1 className="book-title">{book.title}</h1>
-        <h3 className="book-author">Autor: {book.author}</h3>
-        <p className="book-description">{book.description}</p>
-        <p className="book-category">Kategoria: {book.category}</p>
+    <div className="book-container">
+      <div className="book-details">
+        <img className="book-cover" src={book.cover_image} alt={book.title} />
+        <div className="book-info">
+          <h1 className="book-title">{book.title}</h1>
+          <h3 className="book-author">Autor: {book.author}</h3>
+          {renderDescription()}
+          <p className="book-category">Kategoria: {book.category}</p>
+        </div>
       </div>
-      <CommentsSection bookId={id} />
+      <div className="comments-section">
+        <CommentsSection bookId={id} />
+      </div>
     </div>
   );
 };
